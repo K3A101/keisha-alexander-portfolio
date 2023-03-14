9f592436dc2f4b75070cefea11b347a27370010e
K3A101 <keisha.alexander@hva.nl>
2023-03-14 21:47:03 +0100

    api gefetch voor de gebruiker

--- script/main.js
+++ script/main.js
@@ -1,11 +1,13 @@
 const API_URL = "https://api.github.com"
 const username = "K3A101"
-const accessToken = "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"
+const accessToken = "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"
 const repoList = document.getElementById('repository-list');
-
+const userSection = document.getElementById('user');
+const userButton = document.querySelector('main section:nth-of-type(1) button')
 fetchRepoData()
+fetchUserData()
 
- function fetchRepoData() {
+function fetchRepoData() {
     fetch(`${API_URL}/users/${username}/repos`, {
         headers: {
             Authorization: `token ${accessToken}`
@@ -19,23 +21,23 @@ fetchRepoData()
                 const githubPages = repo.homepage;
                 const starredRepo = repo.stargazers_count;
                 const programmingLanguage = repo.language;
-              
-                if (starredRepo != 0) {
-                 const REPO_DOCS_URL = `${API_URL}/repos/${username}/${repoName}/readme`
-                            fetch(REPO_DOCS_URL, {
-                                headers: {
-                                    Authorization: `token ${accessToken}`
-                                }
-                            })
-                            .then((response)=> response.json())
-                            .then((readmes) => {
-                                const readmePage = readmes.html_url
-                               let repoElement = displayRepoData(repoName, repoDescription,  githubPages, readmePage, programmingLanguage)
-                               repoList.insertAdjacentHTML('beforeend', repoElement);  
-                            })
-                                .catch((error) => { console.error(error) });                        
 
-                       
+                if (starredRepo != 0) {
+                    const REPO_DOCS_URL = `${API_URL}/repos/${username}/${repoName}/readme`
+                    fetch(REPO_DOCS_URL, {
+                        headers: {
+                            Authorization: `token ${accessToken}`
+                        }
+                    })
+                        .then((response) => response.json())
+                        .then((readmes) => {
+                            const readmePage = readmes.html_url
+                            let repoElement = displayRepoData(repoName, repoDescription, githubPages, readmePage, programmingLanguage)
+                            repoList.insertAdjacentHTML('beforeend', repoElement);
+                        })
+                        .catch((error) => { console.error(error) });
+
+
                 }
             });
 
@@ -44,7 +46,7 @@ fetchRepoData()
 }
 
 
-function displayRepoData(repoName, repoDescription,  githubPages, readmePage, programmingLanguage) {
+function displayRepoData(repoName, repoDescription, githubPages, readmePage, programmingLanguage) {
     return `
              <article>
                 <h1>${repoName}</h1>
@@ -56,100 +58,46 @@ function displayRepoData(repoName, repoDescription,  githubPages, readmePage, pr
                  </ul>           
              </article>
              `;
-    
+
 }
 
 
+function fetchUserData() {
+    fetch(`${API_URL}/users/${username}`, {
+        headers: {
+            Authorization: `token ${accessToken}`
+        }
+    })
+        .then((response) => response.json())
+        .then((user) => {
+            console.log(user);
+            displayUserData(user)
+        })
+        .catch((error) => {
+            console.error(error)
+        })
+}
 
-
-
-// console.log('hello')
-// const REPO_URL = "https://api.github.com/users/K3A101/repos";
-// const USER_API = "https://api.github.com/users/K3A101";
-// const repoList = document.getElementById('repository-list');
-// const userSection = document.getElementById('user')
-
-// fetchData()
-// fetchUserData()
-
-
-
-// function fetchData() {
-//     fetch(REPO_URL)
-//         .then((response) => response.json())
-//         .then((data) => {
-//             // console.log(data[5].name)
-//             displayRepositories(data)
-//         })
-//         .catch((error) => {
-//             console.error(error)
-//         })
-// }
-
-
-// function displayRepositories(data) {
-//     let repositories = ``
-
-//     data.forEach(item => {
-//         if (item.stargazers_count !== 0) {
-//             repositories = `
-//            <article>
-//            <h1>${item.name}</h1>
-//            <p>${item.description}</p>
-//             <iframe src="${item.homepage}" frameborder="0" width="100%" height="400"></iframe>
-//             <ul>
-//                 <li><a href="#">README</a></li>
-//                 <li>Commits</li>
-//                 <li>issue</li>
-//                 <li><a href="${item.homepage}">Demo</a></li>
-//                 </ul>
-//            </article>`
-
-
-
-//                 ;
-
-
-
-//             repoList.insertAdjacentHTML('beforeend', repositories)
-//         }
-
-//     })
-
-//     // eerstePagina.insertAdjacentHTML('beforeend', repositories)
-// }
-
-
-
-// function fetchUserData() {
-// fetch(USER_API)
-// .then((response)=> response.json())
-// .then((user) => {
-//  console.log(user);
-//  displayUserData(user)
-// })
-// .catch((error)=>{
-//     console.error(error)
-// })
-// }
-
-// function displayUserData(user) {
-//     let userHTML =``
-//     userHTML = `
+function displayUserData(user) {
+    let userHTML = ``
+    userHTML = `
     
-//           <article>
-//                 <img src="${user.avatar_url}" alt="gebruikers foto">
-//                 <p>${user.login}</p>
-//                 <p>${user.name}</p>
-//                 <p>${user.bio}</p>
-//             </article>
-//             <a href="${user.html_url}">Github Account</a>
-//             <figure>
-//                 <img src="#" alt="HTML banner">
-//                 <img src="#" alt="CSS banner">
-//                 <img src="#" alt="Js Banner">
-//             </figure>
-//     `;
-//     userSection.insertAdjacentHTML('beforeend', userHTML)
-   
-// }
+          <article>
+                <img src="${user.avatar_url}" alt="gebruikers foto">
+                <section>
+                <p>${user.login}</p>
+                <p>${user.name}</p>
+                <p>${user.bio}</p>
+                </section>
+                
+            </article>
+            <a href="${user.html_url}">Github Account</a>
+            <figure>
+                <img src="https://camo.githubusercontent.com/d63d473e728e20a286d22bb2226a7bf45a2b9ac6c72c59c0e61e9730bfe4168c/68747470733a2f2f696d672e736869656c64732e696f2f62616467652f48544d4c352d4533344632363f7374796c653d666f722d7468652d6261646765266c6f676f3d68746d6c35266c6f676f436f6c6f723d7768697465" alt="HTML banner">
+                <img src="https://camo.githubusercontent.com/3a0f693cfa032ea4404e8e02d485599bd0d192282b921026e89d271aaa3d7565/68747470733a2f2f696d672e736869656c64732e696f2f62616467652f435353332d3135373242363f7374796c653d666f722d7468652d6261646765266c6f676f3d63737333266c6f676f436f6c6f723d7768697465" alt="CSS banner">
+                <img src="https://camo.githubusercontent.com/93c855ae825c1757f3426f05a05f4949d3b786c5b22d0edb53143a9e8f8499f6/68747470733a2f2f696d672e736869656c64732e696f2f62616467652f4a6176615363726970742d3332333333303f7374796c653d666f722d7468652d6261646765266c6f676f3d6a617661736372697074266c6f676f436f6c6f723d463744463145" alt="Js Banner">
+            </figure>
+    `;
+    userSection.insertAdjacentHTML('beforeend', userHTML)
+
+}
