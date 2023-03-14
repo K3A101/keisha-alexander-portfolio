4a017abd1d4998f93c44654d60d488440ebfac30
K3A101 <keisha.alexander@hva.nl>
2023-03-14 12:56:53 +0100

    Data ophalen van de ropository en gebruiker

--- script/main.js
+++ script/main.js
@@ -1,17 +1,20 @@
 console.log('hello')
-const API_URL = "https://api.github.com/users/K3A101/repos";
-const eerstePagina = document.getElementById('pagina-1');
+const REPO_URL = "https://api.github.com/users/K3A101/repos";
+const USER_API = "https://api.github.com/users/K3A101";
+const repoList = document.getElementById('repository-list');
+const userSection = document.getElementById('user')
 
 fetchData()
+fetchUserData()
 
 
 
 function fetchData() {
-    fetch(API_URL)
+    fetch(REPO_URL)
         .then((response) => response.json())
         .then((data) => {
             // console.log(data[5].name)
-            displayData(data)
+            displayRepositories(data)
         })
         .catch((error) => {
             console.error(error)
@@ -19,14 +22,69 @@ function fetchData() {
 }
 
 
-function displayData(data) {
+function displayRepositories(data) {
     let repositories = ``
 
     data.forEach(item => {
-        repositories = ` <article>${item.name}</article>`;
-        eerstePagina.insertAdjacentHTML('beforeend', repositories)
+        if (item.stargazers_count !== 0) {
+            repositories = `
+           <article>
+           <h1>${item.name}</h1>
+           <p>${item.description}</p>
+            <ul>
+                <li><a href="#">README</a></li>
+                <li>Commits</li>
+                <li>issue</li>
+                <li><a href="${item.homepage}">Demo</a></li>
+                </ul>
+           </article>`
+                ;
+
+
+
+            repoList.insertAdjacentHTML('beforeend', repositories)
+        }
+
     })
 
-    console.log(repositories)
     // eerstePagina.insertAdjacentHTML('beforeend', repositories)
+}
+
+
+
+function fetchUserData() {
+fetch(USER_API)
+.then((response)=> response.json())
+.then((user) => {
+ console.log(user);
+ displayUserData(user)
+})
+.catch((error)=>{
+    console.error(error)
+})
+}
+
+function displayUserData(user) {
+    let userHTML =``
+   
+    userHTML = `
+    
+          <article>
+                <img src="${user.avatar_url}" alt="gebruikers foto">
+                <p>${user.login}</p>
+                <p>${user.name}</p>
+                <p>${user.bio}</p>
+            </article>
+
+            <a href="${user.html_url}">Github Account</a>
+
+            <figure>
+                <img src="#" alt="HTML banner">
+                <img src="#" alt="CSS banner">
+                <img src="#" alt="Js Banner">
+            </figure>
+    `;
+
+    userSection.insertAdjacentHTML('beforeend', userHTML)
+   
 }
