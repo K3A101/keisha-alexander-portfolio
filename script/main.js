6d4c66268c960655af9f6bd2be73814349ec855a
K3A101 <keisha.alexander@hva.nl>
2023-03-15 12:03:48 +0100

    stijling versie 1

--- script/main.js
+++ script/main.js
@@ -23,7 +23,9 @@ function fetchRepoData() {
                     fetch(REPO_DOCS_URL)
                         .then((response) => response.json())
                         .then((readmes) => {
-                            const readmePage = readmes.html_url
+                            const readmePage = readmes.html_url;
+
+                            
                             let repoElement = displayRepoData(repoName, repoDescription, githubPages, readmePage, programmingLanguage)
                             repoList.insertAdjacentHTML('beforeend', repoElement);
                         })
@@ -77,6 +79,7 @@ function displayUserData(user) {
                     <p>${user.name}</p>
                     <p>${user.bio}</p>
                     <a href="${user.html_url}">Github Account</a>
+                    <a href="#">Business card</a>
                     <figure>
                         <img src="https://camo.githubusercontent.com/d63d473e728e20a286d22bb2226a7bf45a2b9ac6c72c59c0e61e9730bfe4168c/68747470733a2f2f696d672e736869656c64732e696f2f62616467652f48544d4c352d4533344632363f7374796c653d666f722d7468652d6261646765266c6f676f3d68746d6c35266c6f676f436f6c6f723d7768697465" alt="HTML banner">
                         <img src="https://camo.githubusercontent.com/3a0f693cfa032ea4404e8e02d485599bd0d192282b921026e89d271aaa3d7565/68747470733a2f2f696d672e736869656c64732e696f2f62616467652f435353332d3135373242363f7374796c653d666f722d7468652d6261646765266c6f676f3d63737333266c6f676f436f6c6f723d7768697465" alt="CSS banner">
