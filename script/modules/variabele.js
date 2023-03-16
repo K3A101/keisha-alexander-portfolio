069c0c869d99a750c6f1fbcf2a6f2023c18e14fc
K3A101 <keisha.alexander@hva.nl>
2023-03-16 11:35:26 +0100

    Code opgesplits in modules

---
+++ script/modules/variabele.js
@@ -0,0 +1,7 @@
+// Variabele voor de API
+export const API_URL = "https://api.github.com"
+export const username = "K3A101"
+// const accessToken = "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"
+export const repoList = document.getElementById('repository-list');
+export const userSection = document.getElementById('user');
+export const userButton = document.querySelector('main section:nth-of-type(1) button')
