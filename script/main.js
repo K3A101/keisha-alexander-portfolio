68a1f255619edb25f044e29cd03edc3f46e00f6c
K3A101 <keisha.alexander@hva.nl>
2023-03-16 11:03:01 +0100

    user container gestijld

--- script/main.js
+++ script/main.js
@@ -60,7 +60,7 @@ function languageColorCode(programmingLanguage) {
     const language = document.querySelectorAll('article ul > li:nth-of-type(1)');
 
     language.forEach((lang) => {
-
+        //   If de programeertaal is css dan voegt deze class toe
         switch (lang.textContent) {
             case 'CSS':
                 console.log(lang);
@@ -103,8 +103,7 @@ function displayUserData(user) {
                     <p>${user.name}</p>
                     <p>${user.login}</p>
                     <p>${user.bio}</p>
-                    <a href="${user.html_url}">Github Account</a>
-                    <a href="#">Business card</a>
+                    <a href="${user.html_url}">Github Profile</a>
                     <figure>
                         <img src="https://camo.githubusercontent.com/d63d473e728e20a286d22bb2226a7bf45a2b9ac6c72c59c0e61e9730bfe4168c/68747470733a2f2f696d672e736869656c64732e696f2f62616467652f48544d4c352d4533344632363f7374796c653d666f722d7468652d6261646765266c6f676f3d68746d6c35266c6f676f436f6c6f723d7768697465" alt="HTML banner">
                         <img src="https://camo.githubusercontent.com/3a0f693cfa032ea4404e8e02d485599bd0d192282b921026e89d271aaa3d7565/68747470733a2f2f696d672e736869656c64732e696f2f62616467652f435353332d3135373242363f7374796c653d666f722d7468652d6261646765266c6f676f3d63737333266c6f676f436f6c6f723d7768697465" alt="CSS banner">
