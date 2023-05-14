99c5b96b44ca3b14ffb6cc9d6466b394592cb9ef
K3A101 <keisha.alexander@hva.nl>
2023-05-14 07:28:15 +0200

    Puntjes op de  I

--- script/project.js
+++ script/project.js
@@ -3,61 +3,66 @@
     const nextButton = document.getElementById('next');
     const prevButton = document.getElementById('previous');
     const sectionImg = document.querySelectorAll(' main div.sections > img');
-const sections = document.querySelectorAll(' main div.sections');
-    // const sections = document.querySelectorAll('div');
     const main = document.querySelector('main');
-    console.log(sections)
 
-  
+    // const sections = document.querySelectorAll('div');
+    
+    
     fetchApi()
-
+    
     function fetchApi() {
         fetch(URL)
-            .then(response => response.json())
-            .then(data => {
-                displayProjectData(data)
-
-
-            })
+        .then(response => response.json())
+        .then(data => {
+            displayProjectData(data)
+            
+            
+        })
     }
+     
 
     function displayProjectData(data) {
         let html = '';
-
         data.forEach(project => {
             html += `
-        <div class="sections">
-         <section>
-                <article>
-                    <h1>${project.name} </h1>
-                    <h2>${project.short_description} </h2>
-                </article>
-
-                   <img src="${project.projectImg}" alt="Schets van mijn poster">
-            </section>
-
+            <div class="sections">
             <section>
-                <article>
-                    <h2>De opdracht </h2>
-                    <p>${project.description}</p>
-
-                    <div class="project-info">
-                        <a href="${project.prototype_link}">Live Demo</a>
-                        <a href="${project.readme_link}">Readme</a>
-                    </div>
+            <article>
+            <h1>${project.name} </h1>
+            <h2>${project.short_description} </h2>
+            </article>
+            
+            <img src="${project.projectImg}" alt="Schets van mijn poster">
+            </section>
+            
+            <section>
+            <article>
+            <h2>De opdracht </h2>
+            <p>${project.description}</p>
+            <ul class="skills">
+            <li>${project.skill_1}</li>
+            <li>${project.skill_2}</li>
+            <li>${project.skill_3}</li>
+            </ul>
+            
+            <div class="project-info">
+            <a href="${project.prototype_link}">Live Demo</a>
+            <a href="${project.readme_link}">Readme</a>
+            </div>
                 
                 </article>
-
+                
                 <img src="${project.sketch_Img}" alt="Schets van mijn poster">
-            </section>
-</div>
-        `
-        })
-        main.insertAdjacentHTML('beforeend', html);
-
-    }
-
-
+                </section>
+                </div>
+                `
+            })
+            main.insertAdjacentHTML('beforeend', html);
+            const sections = document.querySelectorAll(' main div.sections');
+            console.log('section list',sections);  
+        }
+        
+     
 
   
 
