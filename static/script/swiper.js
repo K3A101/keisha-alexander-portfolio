64223641d39ef1a2a8f99642c52af1f9d7f14eaf
K3A101 <alexanderkeisha9@gmail.com>
2025-10-01 14:14:45 +0200

    Slider styling

--- static/script/swiper.js
+++ static/script/swiper.js
@@ -1,8 +1,12 @@
 
  const swiper = new Swiper('.swiper', {
     // Optional parameters
+    spaceBetween: 70,
     direction: 'horizontal',
     loop: true,
+    // autoplay: {
+    //     delay: 5000,
+    //   },
 
     // If we need pagination
     pagination: {
